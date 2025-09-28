e6593d22f8a532b2a4b852e4f5133fba1ab04834
neteronet <neteronet@gmail.com>
2025-09-28 16:10:41 +0800

    changes

--- pages/farmers-logout.php
+++ pages/farmers-logout.php
@@ -1,7 +1,13 @@
 <?php
 session_start();
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-header('Location: farmers-login.php'); // Redirect to login page
-exit();
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// Destroy the session.
+session_destroy();
+
+// Redirect to the login page (or index.php)
+header("location: login.php"); // Or wherever your login page is
+exit;
 ?>
